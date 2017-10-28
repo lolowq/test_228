3fc7ff33b46dad3664697052680b1ac39383b462
Dmitry <dr.d-man54@yandex.ru>
2017-10-28 14:43:10 +0700

    login update
    Теперь можно логиниться, вылогиниваться, и в зависимости от этого будет новая вкладка слева(плохо тестировано, есть косяки)
    Форматирован код

--- ui.R
+++ ui.R
@@ -3,88 +3,69 @@ library(shinydashboard)
 
 dashboardPage(
   dashboardHeader(
-    # disable = TRUE
     title = "Open cinema Russia",
     dropdownMenu(
       type = "notifications",
       icon = icon("question-circle"),
       badgeStatus = NULL,
       headerText = "See also:",
-      notificationItem("shiny", icon = icon("file"),
-                       href = "http://shiny.rstudio.com/"),
-      notificationItem("shinydashboard", icon = icon("file"),
-                       href = "https://rstudio.github.io/shinydashboard/")
+      tags$li(
+        tags$a(
+          href = "https://github.com/lolowq/test_228",
+          target = "_blank", icon("github"), "github"
+        )
+      ),
+      notificationItem(
+        "docs", icon = icon("file"),
+        href = "https://drive.google.com/drive/folders/0BwkvYHu15hIlb0JNV2RnbjdRb1E"
+      )
     ),
     tags$li("My header", class = "dropdown")
   ),
   dashboardSidebar(
-    # actionButton("show", "Login"),
-    # verbatimTextOutput("dataInfo"),
-    #disable = TRUE
-    collapsed = TRUE, #, width="100%"
+    collapsed = TRUE,
     sidebarMenu(
       menuItem("Map", tabName = "map_view", icon = icon("globe")),
-      menuItem("Table city", tabName = "table_city", icon = icon("table"))
-    )
+      menuItem("Table city", tabName = "table_city", icon = icon("table")),
+      menuItem("Authorization", tabName = "registration", icon = icon("sign-in"))
+    ),
+    sidebarMenuOutput("menu")
   ),
   dashboardBody(
-    fluidRow(
-      tabItems(
-        tabItem(
-          tabName = "map_view",
-          #absolutePanel(),
-          # fluidRow(
-          # tabBox(
-          div(class="outer",   
-            tags$head(
-              # Include our custom CSS
-              includeCSS("styles.css"),
-              includeScript("gomap.js")
-            ),
-            leafletOutput("map", width="100%", height="100%"),
-            absolutePanel(
-              id = "controls", class = "panel panel-default", fixed = TRUE,
-              draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
-              width = 330, height = "auto",
-              textInput("dynamic_value","Введите город","Россия"),
-              #h2("Cinema explorer"),
-              #uiOutput("ui"),
-              #verbatimTextOutput("dynamic_value"),
-              #textInput("dynamic_value","","Россия"),
-              uiOutput("ui1")
-            )
+    tabItems(
+      tabItem(
+        tabName = "map_view",
+        div(class = "outer",
+          leafletOutput("map", width = "100%", height = "100%"),
+          absolutePanel(
+            id = "controls", class = "panel panel-default",
+            fixed = TRUE, draggable = TRUE,
+            top = 60, left = "auto", right = 20, bottom = "auto",
+            width = 330, height = "auto",
+            textInput("dynamic_value", "Введите город", "Россия"),
+            uiOutput("ui1")
           )
-        ),
-        tabItem(
-          tabName = "table_city",
-          DT::dataTableOutput("table")
         )
+      ),
+      tabItem(
+        tabName = "table_city",
+        DT::dataTableOutput("table")
+      ),
+      tabItem(
+        class = "active",
+        tabName = "registration",
+        actionButton("show", "Login"),
+        verbatimTextOutput("dataInfo")
+      ),
+      tabItem(
+        tabName = "new",
+        actionButton("show2", "Login2")
+        # TODO : here must be ability to insert new data
       )
+    ),
+    tags$body(
+      includeCSS("styles.css"),
+      includeScript("gomap.js")
     )
   )
 )
-# conditionalPanel("false", icon("crosshair"))
-# ))
-
-# ,
-# 
-#   tabItem(tabName = "new_cinema",
-#           fluidRow(
-# 
-#             box(
-#               title = "Введите файл с кинотеатрами", 
-#               status = "primary", solidHeader = TRUE, width = 2,
-#               collapsible = TRUE,
-#               fileInput('file1', 'Выберите файл с кинотеатрами форматами *.xls или *xlsx',
-#                         accept = c(".xlsx",".xls")
-#               ),
-#               downloadButton("Examle", "Скачать пример файла")
-#             ),
-#             box(
-#               solidHeader = TRUE,
-#               width = 10,
-#               DT::dataTableOutput('table'))
-# 
-# ))
-#)
-# ))

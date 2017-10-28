ac7fc3b690a7841efe5a96880f9b563fb138b280
Dmitry <dr.d-man54@yandex.ru>
2017-10-28 14:46:13 +0700

    login, map move + zoom upgrade
    Перемещение в пределах России, максимальное удаление = 3
    обработчик нажатия на модалку и динамическая обработка нового пункта меню слева в зависимости от того, залогинен юзер или нет
    форматирование кода

--- server.R
+++ server.R
@@ -9,140 +9,146 @@ library(ggmap)
 
 library(googleway)
 key <- "AIzaSyA6s8VCdjeEWlsaQTIxGLnIjfJN9PcLKA0"
+obs1 <<- NULL
 
-# library(rmarkdown)
-# render("../dashboard.Rmd")
-
-Logged <- FALSE
-my_username <- "q"
-my_password <- "q"
+login <- gs_title("user_data")
+login_data <- gs_read(login)
+my_username <- login_data$User
+my_password <- login_data$Pass
 
 gap <- gs_title("Input_data")
 pop <- gs_title("City_population")
 data <- gs_read(gap)
 data_pop <- gs_read(pop)
 data_new <- data
-#write.table(data,'out_data.csv',sep = ";")
-#bob[] <- lapply(bob, as.character)
-# data$Circuit <-lapply(data$Circuit,as.character)
-# data$Circuit[is.na(data$Circuit)] <- ' '
-# translite_cinema <- paste(sep = "<br/>",
-#                           data$Name,
-#                           data$Circuit,
-#                           data$Booker,
-#                           data$Screen)
-#paste(data$Name," " ,data$Circuit," " ,data$Booker, " ", data$Screen)
-# input_data <- function(num){
-#   
-#   switch( num,
-#           read_excel("cinemas.xlsx"),
-#           read_excel("cinemas_all_info.xls")
-#   )
-# }
 
 function(input, output, session) {
-  
+
   output$ui <- renderUI({
     if (is.null(input$select))
       return()
-    lvl_book <- levels(data$Booker)
-    switch(input$select,
-           "City" = textInput("dynamic", "Введите город",
-                              value = "Россия"),
-           "Name" = textInput("dynamic", "Введите название кинотеатра",
-                              value = "Победа")
-           
+    switch(
+      input$select,
+      "City" = textInput(
+        "dynamic",
+        "Введите город",
+        value = "Россия"
+      ),
+      "Name" = textInput(
+        "dynamic",
+        "Введите название кинотеатра",
+        value = "Победа"
+      )
     )
-    
-    
   })
+
   output$ui1 <- renderUI({
-    data_new <- data 
-    clear_data  <- data_new[complete.cases(data_new$Screen),]
+    data_new <- data
+    clear_data  <- data_new[complete.cases(data_new$Screen), ]
     mz <- clear_data$Screen
-    
     min_zal <- min(mz)
     max_zal <- max(mz)
-    
-    sliderInput("range", "Залы кинотеатров",
-                min =min_zal , max = max_zal,step = 1,
-                value = c(min_zal,max_zal))
+    sliderInput(
+      "range", "Залы кинотеатров",
+      min = min_zal, max = max_zal, step = 1,
+      value = c(min_zal, max_zal)
+    )
   })
-  # Create the map
+
+  #---------------\
+  # Create the map )
+  #_______________/
+
   output$map <- renderLeaflet({
-    
-    data_new$Circuit <-lapply(data_new$Circuit,as.character)
-    data_new$Circuit[is.na(data_new$Circuit)] <- ' '
-    translite_cinema <- paste(sep = c("<center/>","<br/>"),
-                              data_new$Name,
-                              data_new$Circuit,
-                              data_new$Screen)
-    if(input$dynamic_value == "Россия" )
-    {    
-      ZOOM=3
-      LAT=50
-      LONG=100
-    }else{
-      # target_pos=geocode(input$dynamic_value)
-      target_pos=google_geocode(address = input$dynamic_value, key = key)
-      LAT=target_pos$results$geometry$location$lat
-      # LONG=target_pos$lon
-      LONG=target_pos$results$geometry$location$lng
-      ZOOM=12
+    data_new$Circuit <- lapply(data_new$Circuit, as.character)
+    data_new$Circuit[is.na(data_new$Circuit)] <- " "
+    translite_cinema <- paste(
+      sep = c("<center/>", "<br/>"),
+      data_new$Name,
+      data_new$Circuit,
+      data_new$Screen
+    )
+    if (input$dynamic_value == "Россия") {
+      ZOOM <- 3
+      LAT <- 50
+      LONG <- 100
+    } else {
+      target_pos <- google_geocode(address = input$dynamic_value, key = key)
+      LAT <- target_pos$results$geometry$location$lat
+      LONG <- target_pos$results$geometry$location$lng
+      ZOOM <- 12
     }
-    
-    
-    leaflet(data) %>%
-      #addProviderTiles(providers$OpenStreetMap.Mapnik) %>%
-      
-      addTiles(
-        urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png"
-      )%>%
-      setView(lng=LONG, lat=LAT, zoom=ZOOM )%>%
-      addMarkers(clusterOptions = markerClusterOptions(),popup = translite_cinema)
-      # fitBounds(lng1 = max(points$long),lat1 = max(points$lat))
-    
+    leaflet(
+      data, options = leafletOptions(
+        minZoom = 3, center = c(50, 100), # preferCanvas = TRUE,
+        maxBounds = list(list(20, -10), list(80, 200))
+      )
+    ) %>%
+    addTiles(urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png") %>%
+    setView(lng = LONG, lat = LAT, zoom = ZOOM ) %>%
+    addMarkers(
+      clusterOptions = markerClusterOptions(),
+      popup = translite_cinema
+    )
   })
-  
-  # leafletProxy("map", data = data_new) %>%
-  #   clearShapes() %>%
-  #   addCircles(~longitude, ~latitude, radius=radius, layerId=~zipcode,
-  #              stroke=FALSE, fillOpacity=0.4, fillColor=pal(colorData)) %>%
-  #   addLegend("bottomleft", pal=pal, values=colorData, title=colorBy,
-  #             layerId="colorLegend")
-  
+
   output$dynamic_value <- renderPrint({
     input$dynamic
   })
-  output$table <- DT::renderDataTable(data_pop,
-                                      options = list(orderClasses = TRUE, pageLength = 100, escape = FALSE))
- 
-  
+  output$table <- DT::renderDataTable(
+    data_pop,
+    options = list(orderClasses = TRUE, pageLength = 100, escape = FALSE)
+  )
+
+
+  #-----------------\
+  # AUTHentification )
+  #_________________/
+
   values <- reactiveValues(authenticated = FALSE)
-  
-  # Return the UI for a modal dialog with data selection input. If 'failed' 
+
+  # Return the UI for a modal dialog with data selection input. If 'failed'
   # is TRUE, then display a message that the previous value was invalid.
   dataModal <- function(failed = FALSE) {
     modalDialog(
+      title = "Authentification",
       textInput("username", "Username:"),
       passwordInput("password", "Password:"),
+      easyClose = TRUE,
       footer = tagList(
-        # modalButton("Cancel"),
-        actionButton("ok", "OK")
+        modalButton("Cancel"),
+        actionButton("ok", "Login")
       )
     )
   }
-  
-  # Show modal when button is clicked.  
+
+  # Show modal when button is clicked.
   # This `observe` is suspended only whith right user credential
-  
-  obs1 <- observe({
-    showModal(dataModal())
+  o <- observeEvent(input$show, {
+    if (values$authenticated) {
+      values$authenticated <- FALSE
+      updateActionButton(session, "show", label = "Login")
+    } else {
+      obs1 <<- observe({
+        showModal(dataModal())
+      })
+    }
+    output$menu <- renderMenu({
+      if (values$authenticated) {
+        return(
+          sidebarMenu(
+            menuItem("new deistvija admin", tabName = "new", icon = icon("calendar"))
+          )
+        )
+      } else {
+        return(
+          sidebarMenu()
+        )
+      }
+    })
   })
-  
   # When OK button is pressed, attempt to authenticate. If successful,
-  # remove the modal. 
-  
+  # remove the modal.
   obs2 <- observe({
     req(input$ok)
     isolate({
@@ -153,23 +159,19 @@ function(input, output, session) {
     Id.password <- which(my_password == Password)
     if (length(Id.username) > 0 & length(Id.password) > 0) {
       if (Id.username == Id.password) {
-        Logged <<- TRUE
         values$authenticated <- TRUE
+        updateActionButton(session, "show", label = "UNLogin")
+        cat("Showing", input$x, "rows\n")
         obs1$suspend()
         removeModal()
-        
       } else {
         values$authenticated <- FALSE
-      }     
+      }
     }
   })
-  
-  
   output$dataInfo <- renderPrint({
     if (values$authenticated) "OK!!!!!"
     else "You are NOT authenticated"
   })
+
 }
-# Search <- "Шатров Никита"
-# 
-# data[which(data$Booker == Search),  ]

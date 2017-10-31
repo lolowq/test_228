a55d130b535beb29eaf798615f1c68ec0adbb235
lolowq <ne.shatrov@gmail.com>
2017-10-31 18:55:58 +0700

    add new type file
    Добавил файлы Димы+подключил таблицу с данными, которую соединили 29.10(Средняя зп+ население)

--- server.R
+++ server.R
@@ -7,108 +7,202 @@ library(readxl)
 library(googlesheets)
 library(ggmap)
 
+library(googleway)
+key <- "AIzaSyA6s8VCdjeEWlsaQTIxGLnIjfJN9PcLKA0"
+obs1 <<- NULL
+
+login <- gs_title("user_data")
+login_data <- gs_read(login)
+my_username <- login_data$User
+my_password <- login_data$Pass
 
 gap <- gs_title("Input_data")
-pop <- gs_title("City_population")
 data <- gs_read(gap)
+
+
+pop <- gs_title("City_population")
 data_pop <- gs_read(pop)
 data_new <- data
-#write.table(data,'out_data.csv',sep = ";")
-#bob[] <- lapply(bob, as.character)
-# data$Circuit <-lapply(data$Circuit,as.character)
-# data$Circuit[is.na(data$Circuit)] <- ' '
-# translite_cinema <- paste(sep = "<br/>",
-#                           data$Name,
-#                           data$Circuit,
-#                           data$Booker,
-#                           data$Screen)
-#paste(data$Name," " ,data$Circuit," " ,data$Booker, " ", data$Screen)
-# input_data <- function(num){
-#   
-#   switch( num,
-#           read_excel("cinemas.xlsx"),
-#           read_excel("cinemas_all_info.xls")
-#   )
-# }
+
+data_city <- gs_title("map_coordinates")
+city_location <- gs_read(data_city)
+city_location_small <- city_location[,c(4,2,9,44,45)]
+
+
 
 function(input, output, session) {
-  
+
   output$ui <- renderUI({
     if (is.null(input$select))
       return()
-  lvl_book <- levels(data$Booker)
-  switch(input$select,
-    "City" = textInput("dynamic", "Введите город",
-                       value = "Россия"),
-    "Name" = textInput("dynamic", "Введите название кинотеатра",
-                       value = "Победа")
-    
-  )
+    switch(
+      input$select,
+      "City" = textInput(
+        "dynamic",
+        "Введите город",
+        value = "Россия"
+      ),
+      "Name" = textInput(
+        "dynamic",
+        "Введите название кинотеатра",
+        value = "Победа"
+      )
+    )
+  })
 
-  
-    })
   output$ui1 <- renderUI({
-    data_new <- data 
-    clear_data  <- data_new[complete.cases(data_new$Screen),]
-  mz <- clear_data$Screen
-  
-  min_zal <- min(mz)
-  max_zal <- max(mz)
-    
-  sliderInput("range", "Залы кинотеатров",
-              min =min_zal , max = max_zal,step = 1,
-              value = c(min_zal,max_zal))
-})
-# Create the map
+    data_new <- data
+    clear_data  <- data_new[complete.cases(data_new$Screen), ]
+    mz <- clear_data$Screen
+    min_zal <- min(mz)
+    max_zal <- max(mz)
+    sliderInput(
+      "range", "Залы кинотеатров",
+      min = min_zal, max = max_zal, step = 1,
+      value = c(min_zal, max_zal)
+    )
+  })
+
+  #---------------\
+  # Create the map )
+  #_______________/
+
   output$map <- renderLeaflet({
-    
-    data_new$Circuit <-lapply(data_new$Circuit,as.character)
-    data_new$Circuit[is.na(data_new$Circuit)] <- ' '
-    translite_cinema <- paste(sep = c("<center/>","<br/>"),
-                              data_new$Name,
-                              data_new$Circuit,
-                              data_new$Screen)
-    if(input$dynamic_value == "Россия" )
-    {    
-      ZOOM=3
-      LAT=50
-      LONG=100
-      }else{
-        target_pos=geocode(input$dynamic_value)
-        LAT=target_pos$lat
-        LONG=target_pos$lon
-        ZOOM=8
+    data_new$Circuit <- lapply(data_new$Circuit, as.character)
+    data_new$Circuit[is.na(data_new$Circuit)] <- " "
+    translite_cinema <- paste(
+      sep = c("<center/>", "<br/>"),
+      data_new$Name,
+      data_new$Circuit,
+      data_new$Screen
+    )
+    if (input$dynamic_value == "Россия") {
+      ZOOM <- 3
+      LAT <- 50
+      LONG <- 100
+    } else {
+      target_pos <- google_geocode(address = input$dynamic_value, key = key)
+      LAT <- target_pos$results$geometry$location$lat
+      LONG <- target_pos$results$geometry$location$lng
+      ZOOM <- 12
     }
-    
-
-    leaflet(data) %>%
-      #addProviderTiles(providers$OpenStreetMap.Mapnik) %>%
-
-      addTiles(
-        urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png"
-      )%>%
-        setView(lng=LONG, lat=LAT, zoom=ZOOM )%>%
-    addMarkers(clusterOptions = markerClusterOptions(),popup = translite_cinema)
-    
+    leaflet(
+      data_new, options = leafletOptions(
+        minZoom = 3, center = c(50, 100), # preferCanvas = TRUE,
+        maxBounds = list(list(20, -10), list(80, 200))
+      )
+    ) %>%
+    addTiles(urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png") %>%
+    setView(lng = LONG, lat = LAT, zoom = ZOOM ) %>%
+    addMarkers(
+      clusterOptions = markerClusterOptions(),
+      popup = translite_cinema
+    )
   })
   
-  # leafletProxy("map", data = data_new) %>%
-  #   clearShapes() %>%
-  #   addCircles(~longitude, ~latitude, radius=radius, layerId=~zipcode,
-  #              stroke=FALSE, fillOpacity=0.4, fillColor=pal(colorData)) %>%
-  #   addLegend("bottomleft", pal=pal, values=colorData, title=colorBy,
-  #             layerId="colorLegend")
-  
+  output$map_city <- renderLeaflet({
+    city_location_small$City <- lapply(city_location_small$City, as.character)
+    #data_new$Circuit[is.na(data_new$Circuit)] <- " "
+    translite_cinema <- paste(
+      sep = c("<center/>", "<br/>"),
+      city_location_small$City,
+      city_location_small$Region,
+      city_location_small$Pop_2016)
+
+    leaflet(
+      city_location_small, options = leafletOptions(
+        minZoom = 3, center = c(50, 100), # preferCanvas = TRUE,
+        maxBounds = list(list(20, -10), list(80, 200))
+      )
+    ) %>%
+      addTiles(urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png") %>%
+      setView(lng = city_location_small$LONG, lat = city_location_small$LAT, zoom =5) %>%
+      addMarkers(
+        clusterOptions = markerClusterOptions(),
+        popup = translite_cinema
+      )
+  })
+
   output$dynamic_value <- renderPrint({
     input$dynamic
   })
-  output$table <- DT::renderDataTable(data_pop,
-                                      options = list(orderClasses = TRUE, pageLength = 100, escape = FALSE))
+  output$table <- DT::renderDataTable(
+    data_pop,
+    options = list(orderClasses = TRUE, pageLength = 100, escape = FALSE)
+  )
+
+
+  #-----------------\
+  # AUTHentification )
+  #_________________/
+
+  values <- reactiveValues(authenticated = FALSE)
+
+  # Return the UI for a modal dialog with data selection input. If 'failed'
+  # is TRUE, then display a message that the previous value was invalid.
+  dataModal <- function(failed = FALSE) {
+    modalDialog(
+      title = "Authentification",
+      textInput("username", "Username:"),
+      passwordInput("password", "Password:"),
+      easyClose = TRUE,
+      footer = tagList(
+        modalButton("Cancel"),
+        actionButton("ok", "Login")
+      )
+    )
+  }
+
+  # Show modal when button is clicked.
+  # This `observe` is suspended only whith right user credential
+  o <- observeEvent(input$show, {
+    if (values$authenticated) {
+      values$authenticated <- FALSE
+      updateActionButton(session, "show", label = "Login")
+    } else {
+      obs1 <<- observe({
+        showModal(dataModal())
+      })
+    }
+    output$menu <- renderMenu({
+      if (values$authenticated) {
+        return(
+          sidebarMenu(
+            menuItem("new deistvija admin", tabName = "new", icon = icon("calendar"))
+          )
+        )
+      } else {
+        return(
+          sidebarMenu()
+        )
+      }
+    })
+  })
+  # When OK button is pressed, attempt to authenticate. If successful,
+  # remove the modal.
+  obs2 <- observe({
+    req(input$ok)
+    isolate({
+      Username <- input$username
+      Password <- input$password
+    })
+    Id.username <- which(my_username == Username)
+    Id.password <- which(my_password == Password)
+    if (length(Id.username) > 0 & length(Id.password) > 0) {
+      if (Id.username == Id.password) {
+        values$authenticated <- TRUE
+        updateActionButton(session, "show", label = "UNLogin")
+        cat("Showing", input$x, "rows\n")
+        obs1$suspend()
+        removeModal()
+      } else {
+        values$authenticated <- FALSE
+      }
+    }
+  })
+  output$dataInfo <- renderPrint({
+    if (values$authenticated) "OK!!!!!"
+    else "You are NOT authenticated"
+  })
+
 }
-# Search <- "Шатров Никита"
-# 
-# data[which(data$Booker == Search),  ]
-
-
-
-
--- ui.R
+++ ui.R
@@ -2,69 +2,75 @@ library(leaflet)
 library(shinydashboard)
 
 dashboardPage(
-  dashboardHeader(title = "Open cinema Russia"),
+  dashboardHeader(
+    title = "Open cinema Russia",
+    dropdownMenu(
+      type = "notifications",
+      icon = icon("question-circle"),
+      badgeStatus = NULL,
+      headerText = "See also:",
+      tags$li(
+        tags$a(
+          href = "https://github.com/lolowq/test_228",
+          target = "_blank", icon("github"), "github"
+        )
+      ),
+      notificationItem(
+        "docs", icon = icon("file"),
+        href = "https://drive.google.com/drive/folders/0BwkvYHu15hIlb0JNV2RnbjdRb1E"
+      )
+    ),
+    tags$li("My header", class = "dropdown")
+  ),
   dashboardSidebar(
-    #disable = TRUE
-    menuItem("Map", tabName = "map_view", icon = icon("globe")),
-    menuItem("Table city", tabName = "table_city", icon = icon("table"))
+    collapsed = TRUE,
+    sidebarMenu(
+      menuItem("Map", tabName = "map_view", icon = icon("globe")),
+      menuItem("Map city", tabName = "map_city", icon = icon("globe")),
+      menuItem("Table city", tabName = "table_city", icon = icon("table")),
+      menuItem("Authorization", tabName = "registration", icon = icon("sign-in"))
+    ),
+    sidebarMenuOutput("menu")
   ),
   dashboardBody(
     tabItems(
-      tabItem(tabName = "map_view",
-    #absolutePanel(),
-
-        # fluidRow(
-        # tabBox(
-               div(class="outer",
-
-                   tags$head(
-                     # Include our custom CSS
-                     includeCSS("styles.css"),
-                     includeScript("gomap.js")
-                   ),
-
-                   leafletOutput("map", width="100%", height="100%"),
-                   absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                                 draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
-                                 width = 330, height = "auto",
-                                 textInput("dynamic_value","Введите город","Россия"),
-                          #h2("Cinema explorer"),
-                          #uiOutput("ui"),
-                          #verbatimTextOutput("dynamic_value"),
-                          #textInput("dynamic_value","","Россия"),
-                          uiOutput("ui1")
-                          
-                   )
-
-      )),
-      
-      tabItem(tabName = "table_city",
-               DT::dataTableOutput("table"))
-      
-      )))
-  # conditionalPanel("false", icon("crosshair"))
-      # ))
-      
-      
-      # ,
-      # 
-      #   tabItem(tabName = "new_cinema",
-      #           fluidRow(
-      # 
-      #             box(
-      #               title = "Введите файл с кинотеатрами", 
-      #               status = "primary", solidHeader = TRUE, width = 2,
-      #               collapsible = TRUE,
-      #               fileInput('file1', 'Выберите файл с кинотеатрами форматами *.xls или *xlsx',
-      #                         accept = c(".xlsx",".xls")
-      #               ),
-      #               downloadButton("Examle", "Скачать пример файла")
-      #             ),
-      #             box(
-      #               solidHeader = TRUE,
-      #               width = 10,
-      #               DT::dataTableOutput('table'))
-      # 
-      # ))
-  #)
-# ))
+      tabItem(
+        tabName = "map_view",
+        div(class = "outer",
+          leafletOutput("map", width = "100%", height = "100%"),
+          absolutePanel(
+            id = "controls", class = "panel panel-default",
+            fixed = TRUE, draggable = TRUE,
+            top = 60, left = "auto", right = 20, bottom = "auto",
+            width = 330, height = "auto",
+            textInput("dynamic_value", "Введите город", "Россия"),
+            uiOutput("ui1")
+          )
+        )
+      ),
+      tabItem(
+        tabName = "map_city",
+        leafletOutput("map_city", width = "100%", height = "100%")
+      ),
+      tabItem(
+        tabName = "table_city",
+        DT::dataTableOutput("table")
+      ),
+      tabItem(
+        class = "active",
+        tabName = "registration",
+        actionButton("show", "Login"),
+        verbatimTextOutput("dataInfo")
+      ),
+      tabItem(
+        tabName = "new",
+        actionButton("show2", "Login2")
+        # TODO : here must be ability to insert new data
+      )
+    ),
+    tags$body(
+      includeCSS("styles.css"),
+      includeScript("gomap.js")
+    )
+  )
+)
